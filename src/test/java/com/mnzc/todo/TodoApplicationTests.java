a3069f074bfee99dca673f229e2745deeb1c11fe
Jose Muñoz <miguejose230@gmail.com>
2023-04-03 09:47:48 -0500

    first commit

---
+++ src/test/java/com/mnzc/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnzc.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
